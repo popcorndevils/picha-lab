d9c33c66ad760b8edf54d41747828177c743cf70
Michael Coates <popcorndevils@gmail.com>
2021-10-17 23:06:32 -0400

    First commit after download to new computer

--- Picha Lab/src/Application.cs
+++ Picha Lab/src/Application.cs
@@ -2,52 +2,52 @@ using Godot;
 
 public class Application : Node
 {
-    public override void _Ready()
-    {
-        OS.MinWindowSize = new Vector2(820, 550);
-        this.GetNode<MenuBar>("PichaGUI/WSVert/MenuBar").ItemSelected += this.HandleMenu;
-    }
-    
-    public override void _Process(float delta)
-    {
-        if (Input.IsActionJustPressed("undo"))
-        {
-            this.GetTree().CallGroup("gp_canvas_handler", "UndoChange");
-        }
-        else if (Input.IsActionJustPressed("export_test"))
-        {
-            this.GetTree().CallGroup("gp_canvas_handler", "ExportTest");
-        }
-    }
+	public override void _Ready()
+	{
+		OS.MinWindowSize = new Vector2(820, 550);
+		this.GetNode<MenuBar>("PichaGUI/WSVert/MenuBar").ItemSelected += this.HandleMenu;
+	}
+	
+	public override void _Process(float delta)
+	{
+		if (Input.IsActionJustPressed("undo"))
+		{
+			this.GetTree().CallGroup("gp_canvas_handler", "UndoChange");
+		}
+		else if (Input.IsActionJustPressed("export_test"))
+		{
+			this.GetTree().CallGroup("gp_canvas_handler", "ExportTest");
+		}
+	}
 
-    public void HandleMenu(MenuBarItem menu)
-    {
-        switch(menu.Action)
-        {
-            case "new_canvas":
-                this.GetTree().CallGroup("gp_canvas_handler", "AddCanvas", new GenCanvas());
-                break;
-            case "open_canvas":
-                this.GetTree().CallGroup("diag_file", "OpenDialog", DialogMode.OPEN_CANVAS);
-                break;
-            case "save_canvas":
-                this.GetTree().CallGroup("gp_canvas_handler", "Save");
-                break;
-            case "save_canvas_as":
-                this.GetTree().CallGroup("gp_canvas_handler", "SaveCanvas");
-                break;
-            case "open_docs":
-                this.GetTree().CallGroup("diag_help", "OpenHelp");
-                break;
-            case "export_sprite":
-                this.GetTree().CallGroup("gp_canvas_handler", "ExportCanvas");
-                break;
-            case "about_picha":
-                this.GetTree().CallGroup("diag_about", "OpenAbout");
-                break;
-            default:
-                GD.PrintErr($"Unable to Parse MenuItem action \"{menu.Action}\".");
-                break;
-        }
-    }
+	public void HandleMenu(MenuBarItem menu)
+	{
+		switch(menu.Action)
+		{
+			case "new_canvas":
+				this.GetTree().CallGroup("gp_canvas_handler", "AddCanvas", new GenCanvas());
+				break;
+			case "open_canvas":
+				this.GetTree().CallGroup("diag_file", "OpenDialog", DialogMode.OPEN_CANVAS);
+				break;
+			case "save_canvas":
+				this.GetTree().CallGroup("gp_canvas_handler", "Save");
+				break;
+			case "save_canvas_as":
+				this.GetTree().CallGroup("gp_canvas_handler", "SaveCanvas");
+				break;
+			case "open_docs":
+				this.GetTree().CallGroup("diag_help", "OpenHelp");
+				break;
+			case "export_sprite":
+				this.GetTree().CallGroup("gp_canvas_handler", "ExportCanvas");
+				break;
+			case "about_picha":
+				this.GetTree().CallGroup("diag_about", "OpenAbout");
+				break;
+			default:
+				GD.PrintErr($"Unable to Parse MenuItem action \"{menu.Action}\".");
+				break;
+		}
+	}
 }
