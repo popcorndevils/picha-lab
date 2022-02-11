dbcde018a2a90b3dfeacae256875d27439415625
Michael Coates <popcorndevils@gmail.com>
2022-02-10 22:13:48 -0500

    Changed key for logs dictionary to the layer itself.

--- src/ui/LayerStack/ChangeLog.cs
+++ src/ui/LayerStack/ChangeLog.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 
 public class ChangeLog : Tree
 {
-    private Dictionary<string, TreeItem> _Logs = new Dictionary<string, TreeItem>();
+    private Dictionary<Layer, TreeItem> _Logs = new Dictionary<Layer, TreeItem>();
     private string _PrevName = null;
     private TreeItem _PrevNameChange;
 
@@ -21,21 +21,15 @@ public class ChangeLog : Tree
 
     public void LogItem(LayerLogEvent log)
     {
+        var _layer = log.Change.Sender;
         TreeItem _node;
 
         // TODO: correct renaming one layer right after another
         if(log.Change.Type == LayerChangeType.NAME)
         {
-            if(this._Logs.ContainsKey(log.Change.OldValue.ToString()))
+            if(this._Logs.ContainsKey(_layer))
             {
-                var _value = this._Logs[log.Change.OldValue.ToString()];
-                this._Logs.Remove(log.Change.OldValue.ToString());
-                this._Logs.Add(log.Change.Sender.Name, _value);
-                _value.SetText(0, log.Change.Sender.Name);
-            }
-
-            if(this._Logs.ContainsKey(log.Change.Sender.Name))
-            {
+                this._Logs[_layer].SetText(0, log.Change.Sender.Name);
                 if(this._PrevName != null)
                 {
                     this._PrevNameChange.SetTooltip(0, $"{this._PrevName} -> {log.Change.Sender.Name}");
@@ -43,7 +37,7 @@ public class ChangeLog : Tree
                 else
                 {
                     this._PrevName = log.Change.OldValue.ToString();
-                    this._PrevNameChange = this.CreateItem(this._Logs[log.Change.Sender.Name]);
+                    this._PrevNameChange = this.CreateItem(this._Logs[_layer]);
                     this._PrevNameChange.SetText(0, log.Label);
                     this._PrevNameChange.SetTooltip(0, log.Description);
                 }
@@ -54,15 +48,15 @@ public class ChangeLog : Tree
         {
             this._PrevName = null;
 
-            if(this._Logs.ContainsKey(log.Change.Sender.Name))
+            if(this._Logs.ContainsKey(_layer))
             {   
-                _node = this.CreateItem(this._Logs[log.Change.Sender.Name]);
+                _node = this.CreateItem(this._Logs[_layer]);
             }
             else
             {
                 var _parent = this.CreateItem(this.GetRoot());
                 _parent.SetText(0, log.Change.Sender.Name);
-                this._Logs[log.Change.Sender.Name] = _parent;
+                this._Logs[_layer] = _parent;
                 _node = this.CreateItem(_parent);
             }
 
